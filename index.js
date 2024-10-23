9b8f732e64daae67bd3e8015b2103d0360165706
Gaurav shokhanda <gauravshokanda2@gmail.com>
2024-10-23 22:02:58 +0530

    addec

--- index.js
+++ index.js
@@ -12,16 +12,8 @@ const app = express();
 dotenv.config();
 
 // Middleware
-const corsOpts = {
-  origin: "*",
-
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-
-  allowedHeaders: ["Content-Type"],
-};
-
+app.use(cors({ origin: "*", credentials: true })); // Allow all origins
 app.use(express.json());
-app.use(cors(corsOpts));
 app.use("/uploads", express.static("uploads")); // Serve video files statically
 
 // Connect to MongoDB
@@ -38,6 +30,7 @@ app.use("/api/auth", authRoutes); // Authentication routes
 app.use("/api/videos", videoRoutes); // Video routes
 app.use("/api/timetracker", timeTrackerRoutes); // Time tracker routes
 app.use("/api/calculations", calculationRoutes);
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
